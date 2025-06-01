61b41f8a76a31216fd77fb2bd983cf24479b6285
Thiago <thiago2525@yahoo.com.br>
2025-06-01 17:29:57 -0300

    criando funcao add tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -38,19 +38,31 @@ function App() {
     setTasks(newTasks)
   }
 
-  function onDeleteTaskClick (taskId) {
+  function onDeleteTaskClick(taskId) {
     const newTasks = tasks.filter(task => task.id != taskId)
     setTasks(newTasks)
   }
 
+  function onAddtaskSubmit(title, description) {
+    const newTasks = {
+      id: tasks.length + 1,
+      title,
+      description,
+      isCompleted: false,
+
+    }
+    setTasks([...tasks, newTasks])
+
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
         <AddTask />
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick} />
       </div>
     </div>
   );
